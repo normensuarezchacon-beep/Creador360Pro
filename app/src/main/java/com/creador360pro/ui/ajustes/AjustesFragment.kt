70faddcb941faf03e190ab807370860826dd2611
normensuarezchacon-beep <normensuarezchacon@gmail.com>
2026-07-27 17:22:29 -0400

    Create AjustesFragment.kt

---
+++ app/src/main/java/com/creador360pro/ui/ajustes/AjustesFragment.kt
@@ -0,0 +1,12 @@
+package com.creador360pro.ui.ajustes
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.creador360pro.R
+class AjustesFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_ajustes, container, false)
+    }
+}
