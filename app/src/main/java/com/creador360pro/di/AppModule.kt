1538456c824739d074f28869344530e939356f5e
normensuarezchacon-beep <normensuarezchacon@gmail.com>
2026-07-28 11:28:36 -0400

    Create AppModule.kt

---
+++ app/src/main/java/com/creador360pro/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.creador360pro.di
+
+import com.creador360pro.data.db.AppDatabase
+import com.creador360pro.data.dao.*
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val appModule = module {
+
+    // Base de datos
+    single { AppDatabase.getInstance(get()) }
+
+    // DAOs
+    single { get<AppDatabase>().ideaDao() }
+    single { get<AppDatabase>().calendarDao() }
+    single { get<AppDatabase>().incomeDao() }
+    single { get<AppDatabase>().contactDao() }
+    single { get<AppDatabase>().collaborationDao() }
+    single { get<AppDatabase>().designProjectDao() }
+    single { get<AppDatabase>().videoProjectDao() }
+}
