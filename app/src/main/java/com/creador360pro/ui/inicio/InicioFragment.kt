b18a8fdc420f6b3b1ea990c4aaf04031714690f4
normensuarezchacon-beep <normensuarezchacon@gmail.com>
2026-07-28 12:15:15 -0400

    Update InicioFragment.kt

--- app/src/main/java/com/creador360pro/ui/inicio/InicioFragment.kt
+++ app/src/main/java/com/creador360pro/ui/inicio/InicioFragment.kt
@@ -1,12 +1,22 @@
 package com.creador360pro.ui.inicio
+
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.creador360pro.R
+import com.creador360pro.ui.editor.DesignEditorActivity
+
 class InicioFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_inicio, container, false)
+        val view = inflater.inflate(R.layout.fragment_inicio, container, false)
+
+        view.findViewById<View>(R.id.btnDiseno).setOnClickListener {
+            startActivity(Intent(requireContext(), DesignEditorActivity::class.java))
+        }
+
+        return view
     }
 }
