8676e59d862ff6af64c022a64d9305d5e37fec7a
normensuarezchacon-beep <normensuarezchacon@gmail.com>
2026-07-28 11:41:06 -0400

    Update AppDatabase.kt

--- app/src/main/java/com/creador360pro/data/db/AppDatabase.kt
+++ app/src/main/java/com/creador360pro/data/db/AppDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.creador360pro.data.dao.*
 import com.creador360pro.data.model.*
 
 @Database(
