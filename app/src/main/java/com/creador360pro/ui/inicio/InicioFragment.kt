aa22aba3d351f663217d1712165f603becb60d88
normensuarezchacon-beep <normensuarezchacon@gmail.com>
2026-07-30 21:02:34 -0400

    Update InicioFragment.kt

--- app/src/main/java/com/creador360pro/ui/inicio/InicioFragment.kt
+++ app/src/main/java/com/creador360pro/ui/inicio/InicioFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.creador360pro.R
 import com.creador360pro.ui.editor.DesignEditorActivity
+import com.creador360pro.ui.editor.VideoEditorActivity
 
 class InicioFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -15,6 +16,10 @@ class InicioFragment : Fragment() {
 
         view.findViewById<View>(R.id.btnDiseno).setOnClickListener {
             startActivity(Intent(requireContext(), DesignEditorActivity::class.java))
+        }
+
+        view.findViewById<View>(R.id.btnVideo).setOnClickListener {
+            startActivity(Intent(requireContext(), VideoEditorActivity::class.java))
         }
 
         return view
